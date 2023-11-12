b4d65a86bf8f77f518c44c0a08f60e48d8802a51
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2023-11-12 12:08:08 -0500

    feat: build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,23 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue, { status: 200 })
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: Number(params.id) },
+  })
+  if (!issue) {
+    return NextResponse.json({ message: 'Invalid Issue' }, { status: 404 })
+  } 
+
+  // delete issue from database
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  })
+
+  return NextResponse.json({})
+
+}
