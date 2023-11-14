e4ef713f0cb3fd1ce2899287dadea491fb9d1ccf
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2023-11-13 21:59:42 -0500

    feat: build IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
 import Pagination from './components/Pagination'
 import LatestIssues from './LatestIssues'
+import IssueSummary from './IssueSummary'
+import prisma from '@/prisma/client'
 
-export default function Home({searchParams}: {searchParams: {page: string}}) {
+export default async function Home({searchParams}: {searchParams: {page: string}}) {
+  const open = await prisma.issue.count({where: {status: 'OPEN'}})
+  const inProgress = await prisma.issue.count({where: {status: 'IN_PROGRESS'}})
+  const done = await prisma.issue.count({where: {status: 'DONE'}})
+
   return (
-    <LatestIssues />
+    <>
+      <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} done={done}/>
+    </>
   )
 }
